a7dcc98269c8266dab9bbb5b58995b474dd2d53a
HPE <seorab@naver.com>
2018-10-23 16:32:11 +0900

    xxx

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Notice
+
+def index(request):
+    notice = Notice.objects.all()
+
+    return render(
+        request, 'app/index.html',
+        {'list': notice})
+
+def detail(request, num):
+    item = Notice.objects.get(id=num)
+    # item = Notice.objects.get(pk=1)
+    return render(
+        request, 'app/detail.html', {'item': item})
